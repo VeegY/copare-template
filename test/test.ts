594a6fd62a1e51b821a05e1177bf34f2f94abf2e
Kevin Hollmann <kh@imagineon.de>
2021-09-27 09:52:20 +0200

    Add basic test case

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import "mocha";
+import {expect} from "chai";
+
+describe("The Test case", function(){
+    it("should compare boolean values as correctly", function(){
+        expect(true).to.be.equal(true);
+    })
+})
